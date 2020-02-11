9074a46f830fa6657e0eb5f49b52c0535131796b
younes <yb74@hotmail.fr>
2020-02-11 11:41:08 +0100

    Rewriting the code properly and deleting useless comments.

--- src/model/Post.php
+++ src/model/Post.php
@@ -5,69 +5,44 @@ class Post {
     private string $content;
     private string $date;
 
-    /**
-     * @return int
-     */
+
     public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     */
     public function setId(int $id): void
     {
         $this->id = $id;
     }
 
-    /**
-     * @return string
-     */
     public function getTitle(): string
     {
         return $this->title;
     }
 
-    /**
-     * @param string $title
-     */
     public function setTitle(string $title): void
     {
         $this->title = $title;
     }
 
-    /**
-     * @return string
-     */
     public function getContent(): string
     {
         return $this->content;
     }
 
-    /**
-     * @param string $content
-     */
     public function setContent(string $content): void
     {
         $this->content = $content;
     }
 
-    /**
-     * @return string
-     */
     public function getDate(): string
     {
         return $this->date;
     }
 
-    /**
-     * @param string $date
-     */
     public function setDate(string $date): void
     {
         $this->date = $date;
     }
-
-
 }
